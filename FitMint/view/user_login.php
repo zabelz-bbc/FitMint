5f5b277125ceb96040755d66d1217f85354c997c
Zakgul <Zakgul@WS-ZAKGUL.bbcnet-zh.local>
2017-03-16 16:16:01 +0100

    Bin noch am Login dran bitte so stehen lassen

--- FitMint/view/user_login.php
+++ FitMint/view/user_login.php
@@ -2,7 +2,7 @@
     <div class="container">
 
       <form class="form-signin">
-        <h2 class="form-signin-heading">Please sign in</h2>
+        <h2 class="form-signin-heading">Login</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
@@ -15,5 +15,22 @@
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
 
-    </div> <!-- /container -->
+  
 
+
+
+      <form class="form-signin1">
+        <h2 class="form-signin-heading">Registrieren</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+  </div> <!-- /container -->
