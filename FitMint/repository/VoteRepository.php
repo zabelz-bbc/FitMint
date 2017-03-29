70298e8108e5d663ed43c9bb638995350e7607a3
Zakgul <Zakgul@WS-ZAKGUL.bbcnet-zh.local>
2017-03-29 15:25:42 +0200

    AnzLike und DisLike in arbeit

--- FitMint/repository/VoteRepository.php
+++ FitMint/repository/VoteRepository.php
@@ -1,42 +1,51 @@
 <?php
 require_once '../lib/Repository.php';
 require_once 'PostArray.php';
-
-class PostRepository extends Repository
-{
+class VoteRepository extends Repository {
 	protected $tableName = 'post';
-
-	 
 	public function getAnzLike($id) {
-
 		$sql = "SELECT anzLike FROM post where id=?";
-		$statement = ConnectionHandler::getConnection()->prepare($sql);
+		$statement = ConnectionHandler::getConnection ()->prepare ( $sql );
 		$statement->bind_param ( 'i', $id ); // eingrenzen was bekommen
-		if (!$statement->execute()) {
-			throw new Exception($statement->error);
+		if (! $statement->execute ()) {
+			throw new Exception ( $statement->error );
 		}
-		$result = $statement -> get_result();
+		$result = $statement->get_result ();
 		if ($result->num_rows > 0) {
-			$row = $result->fetch_assoc();
+			$row = $result->fetch_assoc ();
 			return $row->anzLike;
 		}
 	}
-
-	public function getDislike($id) {
-	
-		$sql = "SELECT anzDislike FROM post where id=?";
-		$statement = ConnectionHandler::getConnection()->prepare($sql);
-		$statement->bind_param ( 'i', $id ); // eingrenzen was bekommen
-		if (!$statement->execute()) {
-			throw new Exception($statement->error);
+	public function setAnzLike($id, $anzLike) {
+		$sql = "UPDATE anzLike FROM post where id=?";
+		$statement = ConnectionHandler::getConnection ()->prepare ( $sql );
+		$statement->bind_param ( 'ii', $anzLike, $id ); // eingrenzen was bekommen
+		if (! $statement->execute ()) {
+			throw new Exception ( $statement->error );
 		}
-		$result = $statement -> get_result();
+	}
+	public function getDislike($id) {
+		$sql = "SELECT anzDislike FROM post where id=?";
+		$statement = ConnectionHandler::getConnection ()->prepare ( $sql );
+		$statement->bind_param ( 'i', $id ); // eingrenzen was bekommen
+		if (! $statement->execute ()) {
+			throw new Exception ( $statement->error );
+		}
+		$result = $statement->get_result ();
 		if ($result->num_rows > 0) {
-			$row = $result->fetch_assoc();
+			$row = $result->fetch_assoc ();
 			return $row->anzDislike;
+		}
+	}
+	public function setAnzDislike($id, $anzDislike) {
+		$sql = "UPDATE anzDislike FROM post where id=?";
+		$statement = ConnectionHandler::getConnection ()->prepare ( $sql );
+		$statement->bind_param ( 'ii', $id, $anzDislike ); // eingrenzen was bekommen
+		if (! $statement->execute ()) {
+			throw new Exception ( $statement->error );
 		}
 	}
 }
 
-
+?>
 
