3e928d7c98aa54c0ef86d204c357c27526560ffc
Zakgul <Zakgul@WS-ZAKGUL.bbcnet-zh.local>
2017-03-29 07:54:48 +0200

    remember me checkbox gelöscht

--- FitMint/view/user_login.php
+++ FitMint/view/user_login.php
@@ -10,10 +10,6 @@
 			for="inputPassword" class="sr-only">Password</label> <input
 			type="password" id="inputPassword" name="password"
 			class="form-control" placeholder="Password" required>
-		<div class="checkbox">
-			<label> <input type="checkbox" value="remember-me"> Remember me
-			</label>
-		</div>
 		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 	</form>
 
@@ -29,10 +25,6 @@
 			for="inputPassword" class="sr-only">Password</label> <input
 			name="password" type="password" id="inputPassword"
 			class="form-control" placeholder="Password bestätigen" required>
-		<div class="checkbox">
-			<label> <input type="checkbox" value="remember-me"> Remember me
-			</label>
-		</div>
 		<button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
 	</form>
 
