4de609af70dc9572fd27599d41f5e7d80d17645b
zabelz <zabelz@WS-ZABELZ.bbcnet-zh.local>
2017-03-30 10:39:21 +0200

    change comment

--- FitMint/view/user_einstellungen.php
+++ FitMint/view/user_einstellungen.php
@@ -1,9 +1,9 @@
+
 <h1>Einstellungen</h1>
-<form class="form-signin-einstellungen" method="post"
-	action="/user/changePassword">
+<form class="form-signin-einstellungen">
 	<h2 class="form-signin-heading">Passwort ändern</h2>
-	<label for="inputPassword" class="sr-only">Password</label> <input
-		type="password" id="inputPassword" class="form-control"
+	<label for="inputEmail" class="sr-only">Email address</label> <input
+		type="email" id="inputEmail" class="form-control"
 		placeholder="altes Passwort" required autofocus> <label
 		for="inputPassword" class="sr-only">Password</label> <input
 		type="password" id="inputPassword" class="form-control"
@@ -11,12 +11,8 @@
 		id="inputPassword" class="form-control"
 		placeholder="Passwort bestätigen" required>
 	<div class="checkbox"></div>
-	<button class="btn btn-lg btn-primary btn-block" type="submit">Passwort
-		ändern</button>
+	<button class="btn btn-lg btn-primary btn-block" type="submit">Passwort ändern</button>
 </form>
-
-
-
 
 <div
 	style="text-align: center; width: 404px; height: 340px; margin-top: 100px; margin-bottom: 24px; margin-left: auto; margin-right: auto">
@@ -28,12 +24,10 @@
 	<script type="text/javascript"><!--
 var fcborder = "ffffff";
 var fcolorbg = "f1edda";
-
+//--></script>
 	<script type="text/javascript"
 		src="http://www.seo-welten.de/tools/color/userinpalette.js"></script>
 </div>
-
-
 
 
 <form action="" method="post" id="form-change-comment">
@@ -45,7 +39,7 @@ var fcolorbg = "f1edda";
 	<div>
 		<p>Hier können sie ihre geschriebenen Kommtenare ändern.</p>
 		<p>$kommentar</p>
-		<textarea rows="5" cols="40" placeholder="changed Comment"></textarea>
+		<textarea rows="5" placeholder="changed Comment"></textarea>
 		<button type="submit" class="btn btn-info">Kommentar speichern</button>
 
 	</div>
