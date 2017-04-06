cd3539728bbd51ad73cad13618a1a11ec318c544
zabelz <zabelz@WS-ZABELZ.bbcnet-zh.local>
2017-04-06 11:14:37 +0200

    Kommentare werden upgedatet, und verlinkung stimmt

--- FitMint/controller/KommentarController.php
+++ FitMint/controller/KommentarController.php
@@ -1,38 +1,34 @@
 <?php
 require_once '../repository/KommentarRepository.php';
 class KommentarController {
-	
 	public function doCreateComment() {
 		$kommentarRepository = new KommentarRepository ();
 		$kommentarRepository->saveComment ( $_SESSION ['loggedInUserId'], $_POST ["postId"], $_POST ["Kommentar"] );
 		header ( "Location: /" ); // Location macht eine neue Anfrage und gelangt zur index Funktion. Diese ruft die Home Seite auf.
 		exit ();
 	}
-
-	
-	public function doUpdateKommentar(){
-		$kommentarRepository = new KommentarRepository();
-		$kommentarRepository->updateKommentar($_POST ["id"], $_POST['inhalt']);
-	}
+	public function doUpdateKommentar() {
+		$kommentarRepository = new KommentarRepository ();
+		$kommentarRepository->updateKommentar ( $_POST ["id"], $_POST ['inhalt'] );
+		header ('Location: /');
 		
-	public function doShowKommentar(){
+	}
+	public function doShowKommentar() {
 		$view = new View ( 'changeKommentar' );
-		$kommentarRepository = new KommentarRepository();
+		$kommentarRepository = new KommentarRepository ();
 		$view->title = '';
 		$view->heading = '';
 		$view->active = 'home';
-		$view->kommentarInhalt = $kommentarRepository->showKommentar($_GET ['kommentarId']);
-		$view->id = $_GET['kommentarId'];
+		$view->kommentarInhalt = $kommentarRepository->showKommentar ( $_GET ['kommentarId'] );
+		$view->id = $_GET ['kommentarId'];
 		$view->display ();
 	}
-	
 	public function getPostId() {
 		$postArray = new PostArray ();
 		$postArray->getPostId ();
 		echo $postArray;
 		return $postArray;
 	}
-	
 	public function doDeleteKommentar() {
 		$kommentarRepository = new KommentarRepository ();
 		$commentId = $_GET ['kommentarId'];
